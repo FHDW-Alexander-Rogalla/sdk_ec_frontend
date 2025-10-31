4a834c05d9adb90a5e6adc9a4ca15b843d53b8d5
FHDW-Alexander-Rogalla <alexander.rogalla@edu.fhdw.de>
2025-10-31 12:00:57 +0200

    Add comments to environment.ts for API connection modes

--- ec_frontend/src/environments/environment.ts
+++ ec_frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5139/api'
+    apiUrl: 'http://localhost:5139/api' /* Connection for when you start the API in HTTP mode */
+    // apiUrl: 'https://localhost:7129/api' /* Connection for when you start the API in HTTPS mode */
 };
