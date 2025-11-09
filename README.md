7c02b79241424e8615346661d6cd861584a995f6
FHDW-Alexander-Rogalla <alexander.rogalla@edu.fhdw.de>
2025-11-09 17:18:41 +0200

    Add support for HTTPS and update Docker configurations
    - Updated Dockerfile to expose port 4443 for HTTPS and modified CMD to include SSL.
    - Added Dockerfile.https for HTTPS support with appropriate configurations.
    - Created compose.https.yaml for running the application with HTTPS.
    - Updated environment.ts to switch API URL for HTTPS mode.
    - Updated README.md to include instructions for HTTP startup.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sdk_ec_frontend
 Software Development Kit, E-Commerce project, Angular-Frontend
+
+
+# HTTP start up
+docker compose -f compose.http.yaml up --build
--- ec_frontend/src/environments/environment.ts
+++ ec_frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5139/api' /* Connection for when you start the API in HTTP mode */
-    // apiUrl: 'https://localhost:7129/api' /* Connection for when you start the API in HTTPS mode */
+    // apiUrl: 'http://localhost:5139/api' /* Connection for when you start the API in HTTP mode */
+    apiUrl: 'https://localhost:7129/api' /* Connection for when you start the API in HTTPS mode */
 };
